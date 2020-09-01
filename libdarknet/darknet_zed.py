d6c9cf27baf992dcc7e50eea1c9b2f803c6454ee
ZafarFaraz2020 <69028699+ZafarFaraz2020@users.noreply.github.com>
2020-09-01 11:11:59 +1000

    Add files via upload
    A successful segmentation mask was implemented using point cloud data

--- libdarknet/darknet_zed.py
+++ libdarknet/darknet_zed.py
@@ -529,7 +529,9 @@ def main(argv):
                 '''depth_var = (depth[y_centroid, x_centroid])  # finding depth of the centre of the bounding box
                 depth_var = math.sqrt((depth_var[0] * depth_var[0]) + (depth_var[1] * depth_var[1]) + (
                         depth_var[2] * depth_var[2])) * 6       #performing a sqrt multiplication to get the euclidian distance of an anchor point in the bounding box as a threshold'''
-                print(depth_var, label)
+                #print(depth_var, label)
+                median_depth = []
+                calc_depth_value = []
                 for i in range(y_extent):  # element by element multiplication of the height of the bounding box
                     y_val = y_coord + (i-1)
                     for j in range(x_extent):  # element by element multiplication of the width of the bounding box
@@ -539,7 +541,24 @@ def main(argv):
                             calc_depth = depth[y_val, x_val]
                             calc_depth = math.sqrt((calc_depth[0] * calc_depth[0]) + (calc_depth[1] * calc_depth[1]) + (
                                 calc_depth[2] * calc_depth[2])) * 10  # calculating euclidian distance of a pixel
-                            if calc_depth < depth_var:  # comparing the pixel distance from the threshold
+                            median_depth = calc_depth
+                            calc_depth_value[y_val,x_val] = calc_depth
+                        except:
+                            pass
+                #print(np.median(median_depth),label)
+                median = np.median(median_depth)
+                for i in range(y_extent):  # element by element multiplication of the height of the bounding box
+                    y_val = y_coord + (i-1)
+                    for j in range(x_extent):  # element by element multiplication of the width of the bounding box
+                        x_val = x_coord + (j-1)
+                        # print(x_val,j)
+                        try:
+                            calc_depth = depth[y_val, x_val]
+
+                            calc_depth = math.sqrt((calc_depth[0] * calc_depth[0]) + (calc_depth[1] * calc_depth[1]) + (
+                                calc_depth[2] * calc_depth[2])) * 10  # calculating euclidian distance of a pixel
+
+                            if calc_depth < median:  # comparing the pixel distance from the threshold
                                 #print("True")
                                 image[y_val, x_val] = (0,255,0,0)
                         except:
@@ -553,9 +572,9 @@ def main(argv):
                 cv2.putText(image, label + " " + (str(distance) + " m"),
                             (x_coord + (thickness * 4), y_coord + (10 + thickness * 4)),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                '''cv2.rectangle(image, (x_coord - thickness, y_coord - thickness),
+                cv2.rectangle(image, (x_coord - thickness, y_coord - thickness),
                               (x_coord + x_extent + thickness, y_coord + y_extent + thickness),
-                              color_array[detection[3]], int(thickness * 2))'''
+                              color_array[detection[3]], int(thickness * 2))
 
 
             cv2.imshow("ZED", image)
@@ -565,7 +584,7 @@ def main(argv):
 
             # log.info("Detection time: {}".format(bench_time - start_time))
             # log.info("Camera FPS: {}".format(1.0 / (time.time() - bench_time)))
-            # log.info("Output FPS: {}".format(1.0 / (time.time() - probs)))
+            log.info("Output FPS: {}".format(1.0 / (time.time() - probs)))
         else:
             key = cv2.waitKey(5)
     cv2.destroyAllWindows()
