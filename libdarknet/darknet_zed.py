43bfd6844e8d44dc8f7b03df5e86ee9dbcac6157
ZafarFaraz2020 <69028699+ZafarFaraz2020@users.noreply.github.com>
2020-10-02 10:38:12 +1000

    Add files via upload
    Added a new file having a new set of functions which have been deleted from the original darknet_zed file

--- libdarknet/darknet_zed.py
+++ libdarknet/darknet_zed.py
@@ -32,6 +32,8 @@ import socket
 
 from threading import Thread
 
+import Bridge
+
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
@@ -80,6 +82,14 @@ class METADATA(Structure):
     _fields_ = [("classes", c_int),
                 ("names", POINTER(c_char_p))]
 
+
+'''
+class DETECTED_MEMORY(Structure):
+    _fields_ = [("label", str),
+                ("x", c_float),
+                ("y", c_float),
+                ("z", c_float),
+                ("Number", c_int)]'''
 
 count = 0
 # lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
@@ -283,29 +293,6 @@ def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45, debug=False):
     return res
 
 
-def socket_server_status(
-        detections,
-        point_cloud_data):  # a socket programme to transmit a certain amount of encrypted data via a TCP or UDP protocol between this program and some other program where the data is needed.
-    # Create a UDP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_address = ('localhost', 10000)
-    message = detections + "//" + point_cloud_data
-
-    try:
-        # Send data
-        sent = sock.sendto(message.encode(), server_address)
-    finally:
-        sock.close()
-
-
-def opfileprint(detection):  # printing the detection into a file for the Overlord to read
-    Fileman = open('YOLO_OUTPUT', 'w')  # creating and opening file in the write configuration
-    for i in detection:
-        Fileman.write(i)  # writing the detection into the file
-        # Fileman.write('\n')
-    Fileman.close()
-
-
 netMain = None
 metaMain = None
 altNames = None
@@ -371,155 +358,8 @@ def generate_color(meta_path):
     return color_array
 
 
-def median_average(median, median_max, median_array):
-    median_max.append(median)
-    #print(len(median_max))
-    while len(median_max) % 10 == 0:
-        median_large = np.mean(median_max)
-        median_max.clear()
-        median_large = float(round(median_large,2))
-        median_array.append(median_large)
-        #print("Values after comparison: ", median_large, median, len(median_max))
-        if abs(median_large - median) > 2.00:
-            median_array.clear()
-        break
-
-    try:
-        return median_array[len(median_array)-1]
-    except:
-        return median
-
-
-def get_median_depth(y_extent, x_extent, y_coord, x_coord, depth, image, median_max, median_array):
-    median_depth = []
-
-    for i in range(y_extent):  # element by element multiplication of the height of the bounding box
-        y_val = y_coord + (i - 1)
-        for j in range(x_extent):  # element by element multiplication of the width of the bounding box
-            x_val = x_coord + (j - 1)
-            # print(x_val,j)
-            try:
-                calc_depth = depth[y_val, x_val]
-                calc_depth = math.sqrt((calc_depth[0] * calc_depth[0]) + (calc_depth[1] * calc_depth[1]) + (
-                        calc_depth[2] * calc_depth[2])) * 10  # calculating euclidian distance of a pixel
-                median_depth = calc_depth
-
-            except:
-                pass
-
-    median = float(round(np.median(median_depth), 2))
-    median_large = median_average(median,median_max,median_array)
-    #print("delay : ",median_large, median)
-
-    for i in range(y_extent):  # element by element multiplication of the height of the bounding box
-        y_val = y_coord + (i - 1)
-        for j in range(x_extent):  # element by element multiplication of the width of the bounding box
-            x_val = x_coord + (j - 1)
-            # print(x_val,j)
-            try:  # encapsulating the depth calculation in a try - catch block to prevent value errors
-                calc_depth = depth[
-                    y_val, x_val]  # storing x,y,z values from individual pixels for comparing with the threshold value
-                calc_depth = math.sqrt(
-                    (calc_depth[0] * calc_depth[0]) + (calc_depth[1] * calc_depth[1]) + (
-                            calc_depth[2] * calc_depth[
-                        2])) * 10  # calculating euclidian distance of a pixel
-
-                if calc_depth < median_large:  # comparing the pixel distance from the threshold
-                    # print("True")
-                    image[y_val, x_val] = (0, 0, 255, -100)
-            except:
-                pass
-    return image
-
-
-def get_color_segmentation_mask(cropped_image, color, mask, y_coord, y_extent, x_coord, x_extent, thresh):
-    cropped_image = cv2.cvtColor(cropped_image,
-                                 cv2.COLOR_BGRA2BGR)  # cropping the image to the size of the bounding box
-    blue = color[0]  # storing blue value in bgr
-    green = color[1]  # storing green value in bgr
-    red = color[2]  # storing red value in bgr
-    try:
-        lower = np.array([(int(blue) - thresh), (int(green) - thresh), (int(red) - thresh)], dtype="uint8")
-        upper = np.array([(int(blue) + thresh), (int(green) + thresh), (int(red) + thresh)], dtype="uint8")
-        masked = cv2.inRange(cropped_image, lower, upper)
-
-        mask[y_coord:(y_coord + y_extent), x_coord:(x_coord + x_extent)] = masked
-    except:
-        mask = mask
-    return mask
-
-
-def get_color_all(image):    # to test the robustness of the color recognition method and can be deleted later if needed
-    image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
-    lower = np.array([1, 90, 200], dtype="uint8")
-    upper = np.array([58, 155, 255], dtype="uint8")
-    # find the colors within the specified boundaries and apply
-    # the mask
-    try:
-        mask = cv2.inRange(image, lower, upper)
-    except:
-        pass
-
-    return mask
-
-
-def get_color(image):
-    img = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)           #converting to a BGR image as the inrange works in only a 3 channel image not a 4 channel
-
-    boundaries = [([17, 15, 100], [50, 56, 234]),  # red
-                  ([36, 31, 4], [180, 88, 60]),  # blue
-                  ([45, 76, 20], [85, 235, 180]),  # green
-                  ([20, 150, 210], [90, 255, 255]),  # yellow
-                  ([103, 86, 65], [145, 133, 128]),     #black
-                  ([1, 1, 1], [45, 40, 40])]        #white
-    count = 0
-    for (lower, upper) in boundaries:
-        try:
-            count += 1
-            # create NumPy arrays from the boundaries
-            lower = np.array(lower, dtype="uint8")
-            upper = np.array(upper, dtype="uint8")
-            # find the colors within the specified boundaries and apply
-            # the mask
-            mask = cv2.inRange(img, lower, upper)
-            if count == 1:
-                red_sum = int(np.sum(mask))  # seperating the values into seperate masks based on color
-            elif count == 2:
-                blue_sum = int(np.sum(mask))
-            elif count == 3:
-                green_sum = int(np.sum(mask))
-            elif count == 4:
-                yellow_sum = int(np.sum(mask))
-            elif count == 5:
-                white_sum = int(np.sum(mask))
-            elif count == 6:
-                black_sum = int(np.sum(mask))
-
-        except:
-            pass
-
-    # placing the sum in an array to be sorted
-
-    color_arrays = [blue_sum, green_sum, red_sum, yellow_sum, white_sum, black_sum]
-    color_arrays = np.sort(color_arrays)            #sorting an array to present the most dominant colour as the output
-    print_color = color_arrays[len(color_arrays) - 1]
-    if print_color == blue_sum:
-        object_color = "Blue"
-    elif print_color == green_sum:
-        object_color = "Green"
-    elif print_color == red_sum:
-        object_color = "Red"
-    elif print_color == yellow_sum:
-        object_color = "Yellow"
-    elif print_color == white_sum:
-        object_color = "White"
-    elif print_color == black_sum:
-        object_color = "Black"
-
-    return object_color             #returning the highest mask value as the colour
-
 def main(argv):
-    thresh = 0.25
+    thresh = 0.7
     darknet_path = "../libdarknet/"
     config_path = darknet_path + "cfg/yolov3.cfg"
     weight_path = "yolov3.weights"
@@ -554,16 +394,19 @@ def main(argv):
 
     input_type = sl.InputType()
     if svo_path is not None:
-        log.info("SVO file : " + svo_path)
+        # log.info("SVO file : " + svo_path)
         input_type.set_from_svo_file(svo_path)
     else:
         # Launch camera by id
         input_type.set_from_camera_id(zed_id)
 
     init = sl.InitParameters(input_t=input_type)
+    init.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
+    init.sdk_verbose = True
     init.coordinate_units = sl.UNIT.METER
-    init.depth_minimum_distance = 0.15
+    init.depth_minimum_distance = 0.20
     init.camera_resolution = sl.RESOLUTION.HD720
+    init.depth_mode = sl.DEPTH_MODE.QUALITY
     init.camera_fps = 60
 
     cam = sl.Camera()
@@ -627,27 +470,40 @@ def main(argv):
     log.info("Running...")
     processes = []
     avg_median = []
+    detected_objects = []
     key = ''
-    while key != 27:  # for 'q' key
+    sensors_data = sl.SensorsData()
+    transform = sl.Transform()
+    tracking_params = sl.PositionalTrackingParameters(transform)  # initialises positional tracking
+    cam.enable_positional_tracking(tracking_params)  # enables positional tracking
+    count_o = 1
+    while key != 113:  # for 'q' key
         point_cloud_data = ""
         probs = time.time()
         err = cam.grab(runtime)
+
+        camera_pose = sl.Pose()
         if err == sl.ERROR_CODE.SUCCESS:
             cam.retrieve_image(mat, sl.VIEW.LEFT)
             image = mat.get_data()
-
-            cam.retrieve_measure(
-                point_cloud_mat, sl.MEASURE.XYZRGBA)
+            py_translation = sl.Translation()
+            cam.retrieve_measure(point_cloud_mat, sl.MEASURE.XYZRGBA)
             depth = point_cloud_mat.get_data()
             # Do the detection
             start_time = time.time()  # start time of the loop
             detections = detect(netMain, metaMain, image, thresh)
-            # opfileprint(str(detections))
+            #Bridge.opfileprint(str(detections))
 
             bench_time = time.time()  # setting checkpoint for the loop
-            mask = np.zeros((image.shape[0], image.shape[1]))
-            # log.info(chr(27) + "[2J" + "**** " + str(len(detections)) + " Results ****")  # printing detected objects
+            mask = np.zeros((image.shape[0], image.shape[1], image.shape[2]))
 
+            tracking_state = cam.get_position(camera_pose,
+                                              sl.REFERENCE_FRAME.WORLD)  # initialises a positional tracking sequence to give the distance moved by the camera using frame world reference
+            if tracking_state == sl.POSITIONAL_TRACKING_STATE.OK:  # activates only when the poisitional tracking state is in 'OK' state
+                tx, ty, tz = Bridge.get_positional_data(camera_pose,
+                                                 py_translation)  # gets translation and rotation data as a string
+
+            # log.info(chr(27) + "[2J" + "**** " + str(len(detections)) + " Results ****")  # printing detected objects
             for detection in detections:
                 label = detection[0]
                 confidence = detection[1]
@@ -661,18 +517,26 @@ def main(argv):
                 y_coord = int(bounds[1] - bounds[3] / 2)
                 # boundingBox = [[x_coord, y_coord], [x_coord, y_coord + y_extent], [x_coord + x_extent, y_coord + y_extent], [x_coord + x_extent, y_coord]]
                 thickness = 1
-
                 x, y, z = get_object_depth(depth, bounds)
+
                 distance = math.sqrt(x * x + y * y + z * z)
                 distance = "{:.2f}".format(distance)
                 distance_data = str(label) + ", position from camera x = " + str(round(x, 2)) + " m,  y= " + str(
                     round(y, 2)) + " m,  z= " + str(round(z, 2)) + " m,"
+                # print(distance_data, label)
+
+                # detected_objects.append(detected_o)
+                # print(detected_o)
+                # print("location data: x: {0}, y: {1}, z: {2} \n".format(x, y, z))
+
+                detected_objects = Bridge.get_detected_objects(detected_objects, label, bounds, x, y, z, camera_pose,
+                                                        py_translation)
 
                 if label == "bicycle":  # a binding statement to direct colour recognition
                     cropped_image = image[y_coord:(y_coord + y_extent), x_coord:(
-                                x_coord + x_extent)]  # cropping image to the size of the object bounding box
+                            x_coord + x_extent)]  # cropping image to the size of the object bounding box
                     # mask[y_coord:(y_coord + y_extent), x_coord:(x_coord + x_extent)] = get_color_all(cropped_image)  # getting the color output from the color recognition function
-                    color_string = get_color(cropped_image)  #getting colour output from the function as a string
+                    color_string = Bridge.get_color(cropped_image)  # getting colour output from the function as a string
                     cropped_image = image[y_coord:(y_coord + y_extent), x_coord:(x_coord + x_extent)]
 
                     try:
@@ -680,25 +544,23 @@ def main(argv):
                     except:
                         color = 0
 
-                    '''thresh_color = 10
-                    mask = get_color_segmentation_mask(cropped_image, color, mask, y_coord, y_extent, x_coord, x_extent,
-                                                       thresh_color)'''         #segmentation based on colour with adaptive threshold range
+                    thresh_color = 10
+                    mask = Bridge.get_color_segmentation_mask(cropped_image, color, mask, y_coord, y_extent, x_coord, x_extent,
+                                                       thresh_color)  # segmentation based on colour with adaptive threshold range
 
                     cv2.putText(image, color_string + " " + label + " " + (str(distance) + " m"),
                                 (x_coord + (thickness * 4), y_coord + (10 + thickness * 4)),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),
                                 2)  # pasting label on top of the segmentation mask
-                if label == "apple":  # if statement to filter the classes needed for segmentation
-
-                    image = get_median_depth(y_extent, x_extent, y_coord, x_coord, depth, image, processes, avg_median)
-
+                if label == "cup":  # if statement to filter the classes needed for segmentation
+                    # print(x, y, z, len(detected_objects))
+                    #image = Bridge.get_median_depth(y_extent, x_extent, y_coord, x_coord, depth, image, processes, avg_median)
                     cv2.putText(image, label + " " + (str(distance) + " m"),
                                 (x_coord + (thickness * 4), y_coord + (10 + thickness * 4)),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),
                                 2)  # pasting label on top of the segmentation mask
 
                 else:  # j += 1
-
                     cv2.putText(image, label + " " + (str(distance) + " m"),  # pasting label on top of detected object
                                 (x_coord + (thickness * 4), y_coord + (10 + thickness * 4)),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
@@ -711,11 +573,13 @@ def main(argv):
             cv2.imshow("ZED", image)
             # cv2.imshow("mask", mask)
             key = cv2.waitKey(5)
-            socket_server_status(str(detections), point_cloud_data)
-
+            print(detected_objects)
+            Bridge.socket_server_status(str(detections), point_cloud_data)
+            # detected_objects.clear()
+            output = time.time() - probs
             # log.info("Detection time: {}".format(bench_time - start_time))
             # log.info("Camera FPS: {}".format(1.0 / (time.time() - bench_time)))
-            #log.info("Output FPS: {}".format(1.0 / (time.time() - probs)))
+            #log.info("Output FPS: {}".format((time.time() - probs)))
         else:
             key = cv2.waitKey(5)
     cv2.destroyAllWindows()
